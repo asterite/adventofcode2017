93e9b8246551d8b9761cbcdaf5a411e313d6f9a9
Ary Borenszweig <asterite@gmail.com>
2017-12-16 13:09:37 -0300

    Day 16: small refactor

--- p16/2.cr
+++ p16/2.cr
@@ -1,19 +1,29 @@
-record Spin, size : Int32
-record Exchange, a : Int32, b : Int32
-record Partner, a : Char, b : Char
+record Spin, size : Int32 do
+  def run(programs)
+    programs.rotate!(-size)
+  end
+end
+
+record Exchange, a : Int32, b : Int32 do
+  def run(programs)
+    programs.swap(a, b)
+  end
+end
+
+record Partner, a : Char, b : Char do
+  def run(programs)
+    programs.swap(programs.index(a).not_nil!, programs.index(b).not_nil!)
+  end
+end
 
 input = File.read("#{__DIR__}/input.txt").strip
 
 instructions = input.split(",").map do |instruction|
   case instruction
-  when %r(s(\d+))
-    Spin.new($1.to_i)
-  when %r(x(\d+)/(\d+))
-    Exchange.new($1.to_i, $2.to_i)
-  when %r(p(\w)/(\w))
-    Partner.new($1[0], $2[0])
-  else
-    raise "Unknown instruction: #{instruction}"
+  when %r(s(\d+))       then Spin.new($1.to_i)
+  when %r(x(\d+)/(\d+)) then Exchange.new($1.to_i, $2.to_i)
+  when %r(p(\w)/(\w))   then Partner.new($1[0], $2[0])
+  else                       raise "Unknown instruction: #{instruction}"
   end
 end
 
@@ -26,21 +36,7 @@ i = 0
 n = 1_000_000_000
 
 while i < n
-  instructions.each do |instruction|
-    case instruction
-    when Spin
-      programs.rotate!(-instruction.size)
-    when Exchange
-      programs.swap(instruction.a, instruction.b)
-    when Partner
-      programs.swap(
-        programs.index(instruction.a).not_nil!,
-        programs.index(instruction.b).not_nil!,
-      )
-    else
-      raise "Unknown instruction: #{instruction}"
-    end
-  end
+  instructions.each &.run(programs)
 
   i += 1
 
